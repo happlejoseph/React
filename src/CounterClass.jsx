a2d1dc6ff581f887f0c030638bbb313e74b264f7
happle joseph <happlejoseph66@gmail.com>
2026-07-03 00:23:36 +0530

    decrement and reset

---
+++ src/CounterClass.jsx
@@ -0,0 +1,36 @@
+import { Component } from "react";
+
+
+class CounterClass extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            count: 0
+        }
+    }
+
+    increment = ()=> {
+        this.setState({count:this.state.count + 1})
+    }
+
+    decrement = ()=> {
+        this.setState({count:this.state.count - 1})
+    }
+
+    reset = ()=> {
+        this.setState({count: 0})
+    }
+
+    render() {
+        return(
+            <div>
+                <h1>Counter Class</h1>
+                <p>Count: {this.state.count}</p>
+
+                <button onClick={this.increment}>increment</button>
+                <button onClick={this.decrement}>decrement</button>
+                <button onClick={this.reset}>reset</button>
+            </div>
+        )
+    }
+}
